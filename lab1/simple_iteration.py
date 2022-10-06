9361e20641469b5363e514ced5f81d97a035646f
Artem Poliakov <artem4250@gmail.com>
2022-10-07 00:49:26 +0300

    simple_iteration.py update

--- lab1/simple_iteration.py
+++ lab1/simple_iteration.py
@@ -5,22 +5,25 @@ def my_phi(x):
     return -log(2-x**2)
 
 
-def simple_iteration(func_phi, x0, q, e):
-    n0 = floor(log(abs(func_phi(x0)-x0)/((1-q)*e)) / log(1/q)) + 1
+def simple_iteration(func_phi, x0, q, eps):
+    n0 = floor(log(abs(func_phi(x0)-x0) / ((1-q) * eps)) / log(1 / q)) + 1
     print("n0(e) >= " + str(n0))
     print("-------------------------------------------------")
 
     x_curr = x0
+    i = 1
     while True:
         x_next = func_phi(x_curr)
-        print("phi(" + str(x_curr) + ") = " + str(x_next))
-        if abs(x_next-x_curr) < (1-q)*e/q:
+        print(str(i) + ": " + str(x_curr) + " ==> " + str(x_next))
+        if abs(x_next-x_curr) < (1-q)*eps/q:
             break
         x_curr = x_next
-    x_next = int(x_next*(10**int(log10(1/e))))*e
+        i += 1
+    x_next = int(x_next * (10 ** int(log10(1 / eps)))) * eps
     print("-------------------------------------------------")
     print("Result is " + str(x_next))
 
 
 if __name__ == '__main__':
-    simple_iteration(my_phi, -1/5, 30/41, 10**(-4))
+    print("\n--- Simple Iteration method ---\n")
+    simple_iteration(my_phi, -3/5, 30/41, 10**(-4))
